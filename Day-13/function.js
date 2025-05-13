16cfdc18b93f40e95ad5f86866b3c9a5b7c6c027
rajesh-rana01 <rajeshra5naa@gmail.com>
2025-05-13 15:34:09 +0545

    update folder

---
+++ Day-13/function.js
@@ -0,0 +1,28 @@
+// function to add two numbers
+
+
+// function addition(num1, num2){
+//     let sum = num1 + num2;
+//     console.log(sum);
+
+// }
+// addition(10,20);
+// addition(100,200);
+
+
+// ***** function to multiply two numbers**
+
+// function multiplication(num1, num2){
+//     let multiply = num1 * num2;
+//     console.log(multiply);
+
+// }
+// multiplication(10,20);
+
+
+// ***** function to greet *****
+
+function Greeting(name){
+    console.log(`Namaste ${name}`);
+}
+Greeting("Rahul");
